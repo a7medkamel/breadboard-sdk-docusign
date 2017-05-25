dc18cc34726f9b319b5460a34b028270298e39b0
Ahmed Kamel <ak@ahmedkamel.net>
2017-05-24 19:29:21 -0700

    initial checkin

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
 var DocuSign  = require('./lib/index')
   , config    = require('config')
+  , _         = require('lodash')
   ;
-
-let proxy = {
-  set: (obj, prop, value) => {
-    return true;
-  },
-  get: (obj, prop) => {
-    if (prop == '$') {
-      return;
-    }
-
-    let ds = obj.$;
-    if (!ds) {
-      ds = obj.$ = new DocuSign();
-    }
-
-    if (prop == 'login') {
-      return ds[prop];
-    }
-
-    return (...args) => {
-      return ds
-              .login()
-              .then((account) => {
-                return ds[prop](...args);
-              });
-    };
-  }
-};
 
 class SDK {
   constructor(options = {}) {
     let { bearer } = options;
+
+    let proxy = {
+      set: (obj, prop, value) => {
+        return true;
+      },
+      get: (obj, prop) => {
+        if (prop == '$') {
+          return;
+        }
+
+        let ds = obj.$;
+        if (!ds) {
+          ds = obj.$ = new DocuSign({ bearer });
+        }
+
+        if (_.has(obj, prop)) {
+          if (prop == 'login') {
+            return ds[prop];
+          }
+
+          if (_.isFunction(ds[prop])) {
+            return (...args) => {
+              return ds
+              .login()
+              .then((account) => {
+                return ds[prop](...args);
+              });
+            };
+          }
+        }
+
+        return ds[prop];
+      }
+    };
 
     this.docusign = new Proxy({}, proxy);
   }
